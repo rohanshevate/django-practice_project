d3765b588577af0d52733f9d2cc9513b67772789
rohanshevate <rohanshevate2001@gmail.com>
2024-11-06 20:11:21 +0530

    display data from database

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import *
-
+from .models import Tasks
 
 # Create your views here.
 def index(request):
-    return render(request,"tasks/list.html")
+    cars = Tasks.objects.all()
+    context = {'tasks' : cars}
+    print(cars)
+    return render(request,"tasks/list.html", context=context)
