3b8d30bee4587a792b17ff488e09ad08aef4587f
rohanshevate <rohanshevate2001@gmail.com>
2024-11-07 20:12:44 +0530

    add functionaliy added

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import *
 from .models import Tasks
+from .forms import  *
 
 # Create your views here.
 def index(request):
     cars = Tasks.objects.all()
-    context = {'tasks' : cars}
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {'tasks': cars, 'form':form}
     print(cars)
-    return render(request,"tasks/list.html", context=context)
+    return render(request, "tasks/list.html", context=context)
