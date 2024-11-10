5bd3b01df08be5a4c22945abe70d9a5a674f4948
rohanshevate <85760038+rohanshevate@users.noreply.github.com>
2024-11-10 07:11:44 +0000

    Delete method created

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.index, name= "index"),
+    #path("update_tasks/<int:pk>",views.update,name="update_tasks"),
+    path("delete/<int:pk>",views.delete,name="delete")
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,7 +4,7 @@ from .forms import  *
 
 # Create your views here.
 def index(request):
-    cars = Tasks.objects.all()
+    car = Tasks.objects.all()
     form = TaskForm()
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -12,6 +12,42 @@ def index(request):
             form.save()
         return redirect("/")
 
-    context = {'tasks': cars, 'form':form}
-    print(cars)
+    context = {'tasks': car, 'form':form}
+    print(car)
     return render(request, "tasks/list.html", context=context)
+
+# def update(request,pk):
+#     task = Tasks.objects.get(id=pk)
+#     form = TaskForm(instance=task)
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST,instance=task)
+#         if form.is_valid():
+#             form.save()
+#         return redirect("/")
+#     context = {'task': task, 'form':form}
+#     return render(request, "tasks/update_tasks.html", context=context)
+
+def delete(request,pk):
+    task = Tasks.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect("/")   
+    context = {'task':task}     
+    return render(request, "tasks/delete.html", context=context)
+
+from django.shortcuts import get_object_or_404, redirect
+
+
+# def delete(request, pk):
+#     # Fetch the task instance to be deleted
+#     task = get_object_or_404(Tasks, id=pk)
+    
+#     # Check if the request method is POST
+#     if request.method == 'POST':
+#         task.delete()
+#         return redirect('/')
+    
+#     # Render a confirmation page if the method is GET
+#     return render(request, 'tasks/delete.html', {'task': task})
+
+
